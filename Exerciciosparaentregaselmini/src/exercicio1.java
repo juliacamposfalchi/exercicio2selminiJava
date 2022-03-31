9b95993135ff9bb29208758aae91add19bd5e16f
juliacamposfalchi <100289636+juliacamposfalchi@users.noreply.github.com>
2022-03-31 14:03:50 -0300

    proximo exercicio selmini

---
+++ Exerciciosparaentregaselmini/src/exercicio1.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+// Exercício 1: Escreva um programa em Java que leia inicialmente as dimensões da matriz (número 
+//de linhas e número de colunas) e, em seguida, faça o preenchimento da matriz com valores inteiros 
+//fornecidos pelo usuário. Após o preenchimento da matriz deverá ser lido via teclado um valor X e 
+//o seu programa deverá fazer a busca desse valor na matriz. Se o valor for encontrado deverá ser 
+//impresso no vídeo a sua localização (número da linha e número da coluna). Caso o valor não esteja 
+//na matriz deverá ser impresso no vídeo a mensagem: “X não se encontra na matriz”. 
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		int x,y,w;
+		
+		System.out.println("Determine um número inteiro para ser o número de linhas da matriz:");
+		x = teclado.nextInt();
+		
+		System.out.println("Determine um número inteiro para ser o número de colunas da matriz: ");
+		y = teclado.nextInt();
+		
+		int k [][] = chamarmatriz (x,y);
+		checkagem (x,y, k); 
+	}		
+		
+		
+		public static int[][] chamarmatriz(int x, int y){
+			
+			Scanner teclado = new Scanner (System.in);
+		
+			int[][] z = new int [x][y];
+			System.out.println("Por favor, digite um número inteiro para a posição de cada canto da matriz!");
+			
+			for(int i = 0; i < x ; i++) {
+				for(int j = 0; j < y ; j++) {
+					System.out.println("Para a linha:" + (i + 1) + "  para a coluna: " + (j + 1));
+						z [i][j]= teclado.nextInt();
+				}
+					
+			} for(int i = 0; i < x ; i++) {
+				for(int j = 0; j < y ; j++) {
+				System.out.print(z[i][j] + "  ");
+				
+				} System.out.println();
+			} return z; }
+		
+		public static void checkagem(int x, int y, int [][] z ) {
+			
+			Scanner teclado = new Scanner(System.in);
+			int w;
+
+			 System.out.println("Qual número dentro da matriz você quer a posição:");
+				w = teclado.nextInt();
+				boolean check = true;
+				for(int i = 0; i < x ; i++) { 
+					for(int j = 0; j < y ; j++) {
+						if (w == z[i][j]) { System.out.println("A posição é:" + i + ","+ j);
+						check = false;	}
+					
+					}						
+				}
+			if (check == true) {
+				System.out.println("Valor não encontrado na matriz");
+			}
+			
+		}
+				
+}			
---
+++ Exerciciosparaentregaselmini/src/exercico2.java
@@ -0,0 +1,61 @@
+import java.util.Random;
+
+/*Escreva um programa em Java que preencha uma matriz quadrada de ordem 4 com 
+valores inteiros aleatórios (gerados a partir da classe Random) e, em seguida, faça a impressão dos 
+elementos no formato de uma tabela (impressão como apresentado durante as aulas). Após a 
+impressão o seu programa deverá trocar os elementos armazenados no triângulo superior da matriz 
+com os elementos armazenados no triângulo inferior. Imprima novamente a matriz no vídeo para 
+conferência. Veja o exemplo abaixo indicando como a troca dos elementos deverá ser realizada. */
+
+public class exercico2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int L [][] = gerarmatriz ();
+		trocarmatriz (L);
+	}
+
+		
+		public static int [][] gerarmatriz (){		
+		Random random = new Random();
+		int[][] z = new int[4][4];
+		
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				int na = random.nextInt(101);
+				z[i][j] = na;
+			}
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				System.out.print(z[i][j] + "  ");
+			}
+			System.out.println();
+		}
+		return z;}
+		
+		public static void trocarmatriz (int L [][]) {
+			int aux;
+			int z [][] = L;
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j <= i; j++) {
+				if (i != j) {
+					aux = z[i][j];
+					z[i][j] = z[j][i];
+					 z[j][i]= aux;
+				}
+			}
+
+		}
+		System.out.println();
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				System.out.print(z[i][j] + "  ");
+			}
+			System.out.println();
+		}
+	}
+
+}
